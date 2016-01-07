b6bd99ad360522610963e32b30cf40953f1a4977
jun.wang <jun.wang@jimubox.com>
2016-01-07 17:47:06 +0800

    feature(ext): fexible questions

--- app/controllers/ext_controller.js
+++ app/controllers/ext_controller.js
@@ -1,12 +1,16 @@
 var controllers;
-var helpers = require('../helpers');
+var fs = require('fs');
 
-var _ = require('lodash');
+var helpers = require('../helpers');
 
 exports.render_list = function (req, res, next) {
     res.render('ext/list', {});
 };
 
 exports.render_questions = function (req, res, next) {
-    res.render('ext/questions', {});
+    var data = require('../json/questions.js');
+
+    res.render('ext/questions', {
+        questions : JSON.stringify(data)
+    });
 };
